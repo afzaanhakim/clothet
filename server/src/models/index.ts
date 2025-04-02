decd083dc6c4158120e782cec8892922aa8c2b33
Afzaan Hakim <afzaanh@Afzaans-MacBook-2025.local>
2025-04-02 14:43:40 -0400

    add location model and user model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,11 +1,58 @@
-import sequelize from '../config/database';
-import ClothingItem from './ClothingItem';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  logging: process.env.NODE_ENV === 'development' ? console.log : false
+});
+
+// Import models
+import User from './user.model';
+import Category from './category.model';
+import Location from './location.model';
+import Item from './item.model';
+import Image from './image.model';
+import Tag from './tag.model';
 
 // Initialize models
-const models = {
-  ClothingItem
-};
+User.initialize(sequelize);
+Category.initialize(sequelize);
+Location.initialize(sequelize);
+Item.initialize(sequelize);
+Image.initialize(sequelize);
+Tag.initialize(sequelize);
 
-// Export models and sequelize instance
-export { sequelize, models };
-export default models; 
+// Define associations
+User.hasMany(Location, { foreignKey: 'user_id' });
+Location.belongsTo(User, { foreignKey: 'user_id' });
+
+User.hasMany(Item, { foreignKey: 'user_id' });
+Item.belongsTo(User, { foreignKey: 'user_id' });
+
+Category.hasMany(Item, { foreignKey: 'category_id' });
+Item.belongsTo(Category, { foreignKey: 'category_id' });
+
+Location.hasMany(Item, { foreignKey: 'location_id' });
+Item.belongsTo(Location, { foreignKey: 'location_id' });
+
+Item.hasMany(Image, { foreignKey: 'item_id' });
+Image.belongsTo(Item, { foreignKey: 'item_id' });
+
+Item.belongsToMany(Tag, { through: 'item_tags', foreignKey: 'item_id' });
+Tag.belongsToMany(Item, { through: 'item_tags', foreignKey: 'tag_id' });
+
+export {
+  User,
+  Category,
+  Location,
+  Item,
+  Image,
+  Tag
+};
---
+++ server/src/models/user.model.ts
@@ -0,0 +1,72 @@
+import {Model, DataTypes, Sequelize, Optional, InferAttributes, InferCreationAttributes, CreationOptional
+} from 'sequelize';
+
+import bcrypt from 'bcrypt';
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+    declare id: CreationOptional<number>;
+    declare email: string;
+    declare password: string;
+    declare name: string;
+    declare created_at: CreationOptional<Date>;
+    declare updated_at: CreationOptional<Date>;
+  
+    // Method to validate password
+    async validatePassword(password: string): Promise<boolean> {
+      return await bcrypt.compare(password, this.password);
+    }
+  
+    static initialize(sequelize: Sequelize) {
+      User.init({
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true
+          }
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        created_at: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+        },
+        updated_at: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+        }
+      }, {
+        sequelize,
+        modelName: 'user',
+        tableName: 'users',
+        underscored: true,
+        hooks: {
+          beforeCreate: async (user) => {
+            if (user.password) {
+              const salt = await bcrypt.genSalt(10);
+              user.password = await bcrypt.hash(user.password, salt);
+            }
+          },
+          beforeUpdate: async (user) => {
+            if (user.changed('password')) {
+              const salt = await bcrypt.genSalt(10);
+              user.password = await bcrypt.hash(user.password, salt);
+            }
+          }
+        }
+      });
+    }
+  }
+
